e2526829aa0829a38007b706cdd555a2e676cfa9
MeHetavi <hetavi.shah05@gmail.com>
2025-02-04 21:01:17 +0530

    .

--- api/api/settings.py
+++ api/api/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+SECRET_KEY = os.getenv("SECRET_KEY", "SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
